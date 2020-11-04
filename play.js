307e1c6c16eee62ea0a344af31296508af2032ba
Charles P <charles.prystupa@gmail.com>
2020-11-04 17:54:59 +0000

    Created client file to hand server connection module

--- play.js
+++ play.js
@@ -1,26 +1,3 @@
-const net = require('net');
-
-
-
-
-
-/**
- * Establishes connection with the game server
- */
-const connect = function() {
-  const conn = net.createConnection({ 
-    host: 'localhost',
-    port: 50541
-  });
-  // interpret incoming data as text
-  conn.setEncoding('utf8'); 
-
-  //Rx Data
-  conn.on('data', (data) =>{console.log('Msg from Server: ', data)});
-
-
-  return conn;
-}
-
+const connect = require('./client.js');
 console.log('Connecting...');
 connect();
