a7eb28f76596bf9c4a7dcc998ff23d19d90c3917
Charles P <charles.prystupa@gmail.com>
2020-11-04 18:06:20 +0000

    Added line to send clients initials to the server

--- client.js
+++ client.js
@@ -1,18 +1,25 @@
 const net = require('net');
+const { hostname } = require('os');
 
 /**
  * Establishes connection with the game server
  */
-const connect = function() {
-  const conn = net.createConnection({ 
+const connect = function () {
+  const conn = net.createConnection({
     host: 'localhost',
     port: 50541
   });
   // interpret incoming data as text
-  conn.setEncoding('utf8'); 
+  conn.setEncoding('utf8');
 
-  //Rx Data
-  conn.on('data', (data) =>{console.log('Msg from Server: ', data)});
+  //Rx Data from Server
+  conn.on('data', (data) => { console.log('Msg from Server: ', data) });
+
+  // Connected Message and send user initials
+  conn.on('connect', () => {
+    console.log(`Connected to game server`)
+    conn.write("Name: CVP")
+  });
 
 
   return conn;
