edb785311c6968943889f2db6f5eca13b64f83bf
Charles P <charles.prystupa@gmail.com>
2020-11-04 20:29:37 +0000

    Corrected alternat svr in constants

--- constants.js
+++ constants.js
@@ -1,10 +1,10 @@
-//Local Server
+// //Local Server
 const IP = 'localhost';
 const PORT = '50541';
 
 //LHL SERVER
-//const IP = '135.23.222.131',
-//const PORT = 50542;
+// const IP = '135.23.222.131';
+// const PORT = '50542';
 
 module.exports = {
   IP,
