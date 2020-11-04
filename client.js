91c13c9932937a59b4f06f0596c9176670a53f59
Charles P <charles.prystupa@gmail.com>
2020-11-04 20:08:31 +0000

    Now sends input over TCP

--- client.js
+++ client.js
@@ -1,5 +1,4 @@
 const net = require('net');
-const { hostname } = require('os');
 
 /**
  * Establishes connection with the game server
@@ -22,15 +21,11 @@ const connect = function () {
   conn.on('connect', () => {
     console.log(`Connected to game server`)
     conn.write("Name: CVP")
-    // conn.write("Move: up")
-    // setTimeout(() => conn.write("Move: left"), 50)
-    // setTimeout(() => conn.write("Move: down"), 100)
-    // setTimeout(() => conn.write("Move: down"), 150)
-    // setTimeout(() => conn.write("Move: right"), 200)
+    
   });
 
 
   return conn;
 }
 
-module.exports = connect;
+module.exports = {connect};
--- input.js
+++ input.js
@@ -1,17 +1,13 @@
-const handleUserInput = function () {
-  process.stdin.on('data', (key) => {
-    // \u0003 maps to ctrl+c input
-    if (key === '\u0003') {
-      process.exit();
-    }
-  })
-};
+
+// Stores the active TCP connection object.
+let connection;
 
 /**
  * Setup User Interface 
  * Specifically, so that we can handle user input via stdin
  */
-const setupInput = function () {
+const setupInput = function (conn) {
+  connection = conn;
   const stdin = process.stdin;
   stdin.setRawMode(true);
   stdin.setEncoding('utf8');
@@ -20,7 +16,30 @@ const setupInput = function () {
   handleUserInput();
   return stdin;
 }
-
+const handleUserInput = function () {
+  process.stdin.on('data', (key) => {
+    // \u0003 maps to ctrl+c input
+    if (key === '\u0003') {
+      process.exit();
+    } else {
+      switch (key) {
+        case 'w':
+          connection.write("Move: up");
+          break;
+        case 'a':
+          connection.write("Move: left");
+          break;
+        case 's':
+          connection.write("Move: down");
+          break;
+        case 'd':
+          connection.write("Move: right");
+          break;
+      }
+    }
+  })
+};
+  
 module.exports = {
   setupInput
 };
